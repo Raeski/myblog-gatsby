e2a83efb7412263be7bd4f7a53bb71560df165b4
Gustavo Raeski <gustavoraeski@outlook.com>
2020-07-12 10:35:42 -0300

    Cleanup starter stuff

---
+++ src/components/SocialLinks/Icons.js
@@ -0,0 +1,11 @@
+import { Github } from '@styled-icons/evaicons-solid/Github'
+import { Linkedin } from '@styled-icons/boxicons-logos/Linkedin'
+import { Facebook } from '@styled-icons/entypo-social/Facebook'
+
+const Icons = {
+    Github,
+    Linkedin,
+    Facebook
+}
+export default Icons
+
---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,17 @@
+const links = [
+    {
+        label: 'Github',
+        url: 'https://github.com/Raeski',
+    },
+    {
+        label: 'Facebook',
+        url: 'https://www.facebook.com/Gustavoraeski/',
+    },
+    {
+        label: 'Linkedin',
+        url: 'https://www.linkedin.com/in/gustavo-raeski',
+    },
+
+]
+
+export default links
